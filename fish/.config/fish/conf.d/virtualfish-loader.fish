a0872e74b9265edec0199c9faecd853b29c7a019
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2024-07-19 10:11:05 +0200

    Fix virtualfish for MacOS

--- fish/.config/fish/conf.d/virtualfish-loader.fish
+++ fish/.config/fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,11 @@
 set -g VIRTUALFISH_VERSION 2.5.9
-set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
-source /home/fscotto/.local/lib/python3.12/site-packages/virtualfish/virtual.fish
+switch (uname -s)
+    case Darwin
+        set -g VIRTUALFISH_PYTHON_EXEC /opt/homebrew/Cellar/virtualfish/2.5.9/libexec/bin/python
+        source /opt/homebrew/Cellar/virtualfish/2.5.9/libexec/lib/python3.12/site-packages/virtualfish/virtual.fish
+    case GNU/Linux
+        set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
+        source $HOME/.local/lib/python3.12/site-packages/virtualfish/virtual.fish
+end
 emit virtualfish_did_setup_plugins
+
