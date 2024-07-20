cbda1baad4ec768141ccf6016214ddf9690a3e88
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2024-07-20 11:36:06 +0200

    Fix Virtualfish env conf

--- fish/.config/fish/conf.d/virtualfish-loader.fish
+++ fish/.config/fish/conf.d/virtualfish-loader.fish
@@ -3,9 +3,8 @@ switch (uname -s)
     case Darwin
         set -g VIRTUALFISH_PYTHON_EXEC /opt/homebrew/Cellar/virtualfish/2.5.9/libexec/bin/python
         source /opt/homebrew/Cellar/virtualfish/2.5.9/libexec/lib/python3.12/site-packages/virtualfish/virtual.fish
-    case GNU/Linux
+    case Linux
         set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
         source $HOME/.local/lib/python3.12/site-packages/virtualfish/virtual.fish
 end
 emit virtualfish_did_setup_plugins
-
