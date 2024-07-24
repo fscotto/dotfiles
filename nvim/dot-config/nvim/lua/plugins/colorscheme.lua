bf056b70c04b861fcc28a5858f75f45946c72159
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2024-07-24 16:06:36 +0200

    Change Neovim theme

--- nvim/dot-config/nvim/lua/plugins/colorscheme.lua
+++ nvim/dot-config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,14 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  "Mofiqul/adwaita.nvim",
+  name = "adwaita",
+  lazy = false,
   priority = 1000,
-  ---@class CatppuccinOptions
-  opts = {
-    transparent_background = false,
-  },
+
+  -- configure and set on startup
+  config = function()
+    vim.g.adwaita_darker = false
+    vim.g.adwaita_disable_cursorline = true
+    vim.g.adwaita_transparent = false
+    vim.cmd("colorscheme adwaita")
+  end,
 }
