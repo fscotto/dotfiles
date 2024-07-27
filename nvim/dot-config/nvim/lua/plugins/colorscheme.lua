c6fcf54362a620153bb5ee2d213de7371ae8e9c1
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2024-07-27 14:08:47 +0200

    Massive change theme to Catppuccin Frappe

--- nvim/dot-config/nvim/lua/plugins/colorscheme.lua
+++ nvim/dot-config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,9 @@
 return {
-  "Mofiqul/adwaita.nvim",
-  name = "adwaita",
+  "catppuccin/nvim",
+  name = "catppuccin",
   lazy = false,
   priority = 1000,
-  -- configure and set on startup
-  config = function()
-    vim.g.adwaita_darker = false
-    vim.g.adwaita_disable_cursorline = true
-    vim.g.adwaita_transparent = false
-    vim.cmd("colorscheme adwaita")
-  end,
+  opts = {
+    transparent_background = false,
+  },
 }
