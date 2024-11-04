edd6540b4cbfbd36dbdd004b43cb48cdae684223
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2024-11-04 22:07:14 +0100

    Enable Neovim Markdown preview

---
+++ nvim/dot-config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,10 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_auto_start = 1
+  end,
+  ft = { "markdown" },
+}
