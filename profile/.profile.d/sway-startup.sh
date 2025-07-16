c1a23dba1073ddfc2613772bcdd95fea48898e79
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2025-07-16 11:13:13 +0200

    Fix Mako configuration

--- profile/.profile.d/sway-startup.sh
+++ profile/.profile.d/sway-startup.sh
@@ -4,6 +4,6 @@ if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
   if [ -n "$DBUS_SESSION_BUS_ADDRESS" ]; then
     exec sway
   else
-    exec dbus-launch sway
+    exec dbus-launch --exit-with-session sway
   fi
 fi
