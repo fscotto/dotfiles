d9743705b0f02672487af7bb91d1c29061f9f8bb
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2025-08-12 15:49:51 +0200

    Fix font on Sway lockscreen

--- sway/.config/sway/scripts/lockman.sh
+++ sway/.config/sway/scripts/lockman.sh
@@ -2,9 +2,9 @@
 
 # Times the screen off and puts it to background
 swayidle \
-	timeout 10 'swaymsg "output * dpms off"' \
-	resume 'swaymsg "output * dpms on"' &
+  timeout 10 'swaymsg "output * dpms off"' \
+  resume 'swaymsg "output * dpms on"' &
 # Locks the screen immediately
-swaylock -f
+swaylock -f --font "Noto Sans Regular" --font-size 16
 # Kills last background task so idle timer doesn't keep running
 kill %%
