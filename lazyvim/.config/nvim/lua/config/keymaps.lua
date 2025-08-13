16f2dbbe3de31c40c4a8a71b906092d9e81c1bdf
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2025-08-13 12:10:58 +0200

    Fix keymaps and fold

--- lazyvim/.config/nvim/lua/config/keymaps.lua
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -9,17 +9,11 @@ map("n", "<leader>bn", ":bnext<CR>", { noremap = true, silent = true, desc = "Ne
 map("n", "<leader>bp", ":bprevious<CR>", { noremap = true, silent = true, desc = "Previous buffer" })
 map("n", "<leader>bd", ":bdelete<CR>", { noremap = true, silent = true, desc = "Delete buffer" })
 
--- Window navigation (splits)
-map("n", "<leader>sh", "<C-w>h", { noremap = true, silent = true, desc = "Window left" })
-map("n", "<leader>sj", "<C-w>j", { noremap = true, silent = true, desc = "Window down" })
-map("n", "<leader>sk", "<C-w>k", { noremap = true, silent = true, desc = "Window up" })
-map("n", "<leader>sl", "<C-w>l", { noremap = true, silent = true, desc = "Window right" })
-
--- Quick save
-map("n", "<leader>w", ":w<CR>", { noremap = true, silent = true, desc = "Save file" })
-
 -- Copy to system clipboard (if not already mapped)
 map({ "n", "v" }, "<leader>y", [["+y]], { noremap = true, silent = true, desc = "Copy to system clipboard" })
 
 -- Paste from system clipboard
 map("n", "<leader>p", [["+p]], { noremap = true, silent = true, desc = "Paste from system clipboard" })
+
+-- Markdown menu
+map("n", "<leader>M", [["+M]], { noremap = true, silent = true, desc = "Markdown" })
