39adacbd5e9e99f09c4e5d46260373209f58acb9
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2025-08-14 09:31:14 +0200

    Restore yank keymap on Neovim

--- lazyvim/.config/nvim/lua/config/keymaps.lua
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -9,6 +9,9 @@ map("n", "<leader>bn", ":bnext<CR>", { noremap = true, silent = true, desc = "Ne
 map("n", "<leader>bp", ":bprevious<CR>", { noremap = true, silent = true, desc = "Previous buffer" })
 map("n", "<leader>bd", ":bdelete<CR>", { noremap = true, silent = true, desc = "Delete buffer" })
 
+-- Copy to system clipboard (if not already mapped)
+map({ "n", "v" }, "<leader>y", [["+y]], { noremap = true, silent = true, desc = "Copy to system clipboard" })
+
 -- Paste from system clipboard
 map("n", "<leader>p", [["+p]], { noremap = true, silent = true, desc = "Paste from system clipboard" })
 
