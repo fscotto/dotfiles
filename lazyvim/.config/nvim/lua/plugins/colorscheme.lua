a1fdb6cb51f0c5e63f3e29233d966280236ad3ac
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2025-08-22 08:29:02 +0200

    Fix Neovim colorscheme
    - Update Neovim plugins
    - Move colorscheme from catppuccin because the theme is broken

--- lazyvim/.config/nvim/lua/plugins/colorscheme.lua
+++ lazyvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -9,6 +9,7 @@ return {
   },
   config = function(_, opts)
     require("catppuccin").setup(opts)
-    vim.cmd.colorscheme("catppuccin")
+    -- vim.cmd.colorscheme("catppuccin")
+    vim.cmd.colorscheme("tokyonight-night")
   end,
 }
--- lazyvim/.config/nvim/lua/plugins/hardtime.lua
+++ lazyvim/.config/nvim/lua/plugins/hardtime.lua
@@ -1,6 +1,7 @@
 return {
-  "m4xshen/hardtime.nvim",
-  lazy = false,
-  dependencies = { "MunifTanjim/nui.nvim" },
-  opts = {},
+        "m4xshen/hardtime.nvim",
+        lazy = false,
+        dependencies = { "MunifTanjim/nui.nvim" },
+        enabled = false,
+        opts = {},
 }
