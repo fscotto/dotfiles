21abf469cb4d673e479379d2432a32a6e30580ae
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2025-08-27 15:47:59 +0200

    Fix Sway script launcher

--- zsh/.profile.d/runwm.sh
+++ zsh/.profile.d/runwm.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
   if [ -n "$DBUS_SESSION_BUS_ADDRESS" ]; then
-    exec dbus-launch --exit-with-session sway
+    exec dbus-run-session sway
   else
     echo "Cannot found dbus session: Sway don't work"
   fi
