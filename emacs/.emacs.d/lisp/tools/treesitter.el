2d510d289c88be36decdaa6969098b8e1a727cb4
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2025-12-25 09:54:19 +0100

    Added YAML and Dockerfile treesitter grammar

--- emacs/.emacs.d/lisp/tools/treesitter.el
+++ emacs/.emacs.d/lisp/tools/treesitter.el
@@ -13,12 +13,16 @@
           (python-mode . python-ts-mode)
           (sh-mode . bash-ts-mode)))
   (setq treesit-language-source-alist
-	'((c "https://github.com/tree-sitter/tree-sitter-c")
+	'((bash "https://github.com/tree-sitter/tree-sitter-bash")
+	  (c "https://github.com/tree-sitter/tree-sitter-c")
           (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
-          (python "https://github.com/tree-sitter/tree-sitter-python")
-          (bash "https://github.com/tree-sitter/tree-sitter-bash")
+	  (dockerfile "https://github.com/camdencheek/tree-sitter-dockerfile")
           (go "https://github.com/tree-sitter/tree-sitter-go")
-	  (gomod "https://github.com/camdencheek/tree-sitter-go-mod"))))
+	  (gomod "https://github.com/camdencheek/tree-sitter-go-mod")
+	  (js "https://github.com/tree-sitter/tree-sitter-javascript")
+	  (json "https://github.com/tree-sitter/tree-sitter-json")
+	  (node "https://github.com/tree-sitter/node-tree-sitter")
+	  (python "https://github.com/tree-sitter/tree-sitter-python"))))
 
 (provide 'treesitter)
 
