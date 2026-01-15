f487fb1b0ac582ff1060180a8f0f024dbeac0674
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2026-01-15 20:38:06 +0100

    Fix Zoxide configuration for Arch

--- bash/.bashrc.d/zoxide.sh
+++ bash/.bashrc.d/zoxide.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if command -v /home/linuxbrew/.linuxbrew/bin/zoxide >/dev/null 2>&1; then
-  eval "$(/home/linuxbrew/.linuxbrew/bin/zoxide init bash)"
+if command -v zoxide >/dev/null 2>&1; then
+  eval "$(zoxide init bash)"
 fi
