370e4816607865ac653d485d28b80e861e7949f5
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2026-02-01 16:35:06 +0100

    Fix Bash config

--- bash/.bashrc.d/java.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if command -v java > /dev/null 2>&1; then
-  JAVA_HOME="$(dirname $(mise bin-paths | grep -i java))"
-  export JAVA_HOME
-fi
--- bash/.bashrc.d/mise.sh
+++ bash/.bashrc.d/mise.sh
@@ -2,5 +2,10 @@
 
 # Load Mise en dev
 if command -v mise > /dev/null 2>&1; then
-  eval "$(mise activate bash)"
+    eval "$(mise activate bash)"
+
+    if mise ls | grep -i java; then
+	JAVA_HOME="$(dirname $(mise bin-paths | grep -i java))"
+	export JAVA_HOME
+    fi
 fi
